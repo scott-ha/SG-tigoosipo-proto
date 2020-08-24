19fba951b7003f3eb1f67038966444a2c885f159
scott-ha <haneul9209@gmail.com>
2020-08-24 17:10:20 +0900

    time setting

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/qr', personal_QR);
-app.use('/gettime', gettime);
+app.use('/time', gettime);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/get_time.js
+++ routes/get_time.js
@@ -7,44 +7,35 @@ moment.tz.setDefault("Asia/Seoul");
 
 const work_week = 40;
 var now_time = moment().format("YYYY년 MM월 DD일 HH시 mm분 ss초");
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(moment().format("YYYY-MM-DD-HH-mm-ss"));//2018-11-18T22:19:20+09:00
-  res.render('index', {
-    title: 'Express'
-  });
-});
 
 // time test hn
-router.post('/time', function(req, res, next) {
+// /time
+router.post('/', function(req, res, next) {
   console.log('--req--');
-  // var a = work_week - now_time;
-  // var time_1 = JSON.parse(req.body.action.params.time);
-  // time_1 = timq_1.value;
-  var a = JSON.parse(req.body.action.params.startTime);
-  console.log(a);
-  console.log(a.value);
-  console.log(typeof a.value);
-  var input_time = moment('03-11', "HH-MM").format();
+  // var a = JSON.parse(req.body);
+  // console.log(a);
+  // console.log(a.value);
+  // console.log(typeof a.value);
+  // var input_time = moment('03-11', "HH-MM").format();
+  var s_time = req.body.action.params.startTime;
+  var e_time = req.body.action.params.endTime;
+  console.log(s_time + "  " + e_time);
+
   kakao_data = {
     "version": "2.0",
     "template": {
-      "outputs": [
-        {
-        "simpleText" : {
-          "text" : "하늘님 " + "현재 시각 " + now_time
-        }
-      },{
+      "outputs": [{
         "simpleText": {
-          "text": input_time
+          "text": "하늘님 " + "현재 시각 " + now_time
         }
       }, {
         "simpleText": {
-          "text": moment().add(3, 'H').format("MM월 DD일 HH시 mm분") + "까지 근무입니다."
+          "text": "시작시간 : " + s_time + "종료시 : " + e_time
         }
       }]
     }
   }
+
   kakao_res = kakao_data;
   res.status(200).send(kakao_res);
   console.log('--res--');
