5408f36b449768fe6cd0ff4499d10e9b92c9c091
VivaanShin <xhatmtjfla23@gmail.com>
2020-08-25 18:06:04 +0900

    get_time update

--- routes/get_time.js
+++ routes/get_time.js
@@ -12,14 +12,26 @@ var now_time = moment().format("YYYY년 MM월 DD일 HH시 mm분 ss초");
 // /time
 router.post('/', function(req, res, next) {
   console.log('--req--');
-  // var a = JSON.parse(req.body);
-  // console.log(a);
-  // console.log(a.value);
-  // console.log(typeof a.value);
-  // var input_time = moment('03-11', "HH-MM").format();
   var s_time = req.body.action.params.startTime;
   var e_time = req.body.action.params.endTime;
-  console.log(s_time + "  " + e_time);
+	var s_time_date = s_time.substring(10, 20);
+	var s_time_time = s_time.substring(21, 26);
+	var s_time = s_time_date + ' ' + s_time_time;
+	var e_time_date = e_time.substring(10, 20);
+	var e_time_time = e_time.substring(21, 26);
+	var e_time = e_time_date + ' ' + e_time_time;
+	console.log('s_time', s_time, typeof s_time);
+
+	//moment
+	var s_time1 = moment(s_time, 'YYYY-MM-DD HH:mm');
+	var e_time1 = moment(e_time, 'YYYY-MM-DD HH:mm');
+
+	var diffTime = {
+		day: moment.duration(e_time1.diff(s_time1)).days(),
+		hour: moment.duration(e_time1.diff(s_time1)).hours(),
+		minute: moment.duration(e_time1.diff(s_time1)).minutes()
+	}
+
 
   kakao_data = {
     "version": "2.0",
@@ -30,12 +42,15 @@ router.post('/', function(req, res, next) {
         }
       }, {
         "simpleText": {
-          "text": "시작시간 : " + s_time + "종료시 : " + e_time
+          "text": "시작시간 : " + s_time + " 종료시간 : " + e_time
+        }
+      }, {
+        "simpleText": {
+          "text": "총 예약 기간 : " + diffTime.day + "일 " + diffTime.hour + "시간 " + diffTime.minute + "분입니다."
         }
       }]
     }
   }
-
   kakao_res = kakao_data;
   res.status(200).send(kakao_res);
   console.log('--res--');
